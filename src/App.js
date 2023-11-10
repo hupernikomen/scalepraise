4b13c7adb3f8481da025fea8b0cdc69fe8119af9
Wilson Ramos <hupcontato@gmail.com>
2023-11-10 14:45:51 -0300

    melhorias

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import moment from 'moment';
 import CalendarItem from "./CalendarItem";
+const tons = require('./utils/canticos.json')
 
 // Louvores
 const praises = {
   friday: ["Tu és soberano", "Maranata", "Meu Barquinho", "Galileu", "Jerusalem"],
   ebd: [],
-  sunday: ["Sobre as aguas", "Maranata", "Galileu", "3 Palavrinhas", "Como José", "Não há Deus maior", "Filho Prodigo", "Filho do Deus vivo", "Reis dos Reis", "Remove a pedra"],
+  sundayprelude: ["Vem, esta é a hora", "Jesus em Tua presença", "Em Espírito em verdade", "Renova-me"],
+  sunday: ["Sobre as águas", "Maranata", "Galileu", "Tu és soberano", "Foi por amor", "Não há Deus maior", "Filho Prodigo", "Ao único",],
+  sundaycommunion: ["Filho do Deus vivo"],
   hcc: ['456', '001', '002', '001', '540'],
-  sundayprelude: ["Vem, esta é a hora", "Jesus em tua presença", "Jesus é o caminho", "Jó"],
-  supper: ["Bendito Cordeiro", "Bendito Cordeiro", "Bendito Cordeiro", "Bendito Cordeiro"] // Louvors de Ceia
+  supper: ["Bendito Cordeiro"] // Louvors de Ceia
 }
 
 // Musicos
 const musicians = {
-  instrumentalists: [],
+  instrumentalists: [
+    
+  ],
   vocals: {
     friday: ["Lidiane", "Laís", "Edvan"],
     ebd: ["Wilson", "Paulinha"],
@@ -21,24 +25,6 @@ const musicians = {
   }
 }
 
-function SelectPraiseSunday(week) {
-  switch (week) {
-    case 1:
-      return [praises.sundayprelude[0], praises.sunday[0], praises.sunday[1]]
-
-    case 2:
-      return [praises.sundayprelude[1], praises.sunday[2], praises.sunday[3]]
-
-    case 3:
-      return [praises.sundayprelude[2], praises.sunday[1], praises.sunday[4]]
-
-    case 4:
-      return [praises.sundayprelude[3], praises.sunday[3], praises.sunday[5]]
-
-    default:
-      return [praises.sundayprelude[4], praises.sunday[4], praises.sunday[6]]
-  }
-}
 
 const scales = [
   {
@@ -58,29 +44,46 @@ const scales = [
     cult: 'Louvor e Pregação',
     praises: SelectPraiseSunday(getWeekNumber()),
     hcc: [praises?.hcc[getWeekNumber() - 1], praises?.hcc[getWeekNumber()]],
-    vocals: ["Edmilson", "Edvan", ...musicians.vocals.sunday] // LOGICA PRA SELECIONAR VOCAIS
+    vocals: ["Edmilson", "Edvan", ...musicians.vocals.sunday], // LOGICA PRA SELECIONAR VOCAIS
+    instrumentalists: ["Wesley", "Annes", ...musicians.instrumentalists.sunday]
   },
-
-
 ]
 
 const otherscales = [
   {
     date: moment("2023/11/13"),
-    cult: 'Especial',
+    cult: 'Culto Especial',
     praises: ["Lovar a Deus"],
     vocals: ["Edmilson", "Edvan", "Duda", "Kelviane"]
   },
 ]
 
+function SelectPraiseSunday(week) {
+  switch (week) {
+    case 1:
+      return [praises.sundayprelude[0], praises.sunday[0], praises.sundaycommunion[0]]
+
+    case 2:
+      return [praises.sundayprelude[1], praises.sunday[2], praises.sundaycommunion[1]]
+
+    case 3:
+      return [praises.sundayprelude[2], praises.sunday[1], praises.sundaycommunion[2]]
+
+    case 4:
+      return [praises.sundayprelude[3], praises.sunday[3], praises.sundaycommunion[3]]
+
+    default:
+      return [praises.sundayprelude[4], praises.sunday[4], praises.sundaycommunion[4]]
+  }
+}
+
+
 function proximoDomingo() {
   return moment().day(7);
 }
-
 function somarDias(data, dias) {
   return moment(data).add(dias, 'days');
 }
-
 function getWeekNumber() {
   var today = new Date();
   var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
@@ -88,7 +91,6 @@ function getWeekNumber() {
   var weekNumber = Math.ceil((pastDaysOfMonth + firstDayOfMonth.getDay() + 1) / 7);
   return weekNumber;
 }
-
 function getSundaysInMonth() {
   var today = new Date();
   var month = today.getMonth();
@@ -104,23 +106,33 @@ function getSundaysInMonth() {
 
   return sundays;
 }
-
 const sortedScales = scales.concat(otherscales).sort((a, b) => {
   return moment(a.date) - moment(b.date);
 });
+const uniqueItems = praises.sunday?.concat(praises.friday, praises.sundayprelude, praises.supper).filter((item, index, array) => {
+  return array.indexOf(item) === index;
+});
+function getTons(n) {
+  for (let i = 0; i < tons.length; i++) {
+    if (tons[i].name === n) return tons[i].tom;
+
+  }
+  return null; // caso não encontre, retorna null
+}
+
+
 
 function ListPraises() {
   return (
     <div>
-
-      <h3 style={{ paddingLeft: 30, fontWeight:500, fontSize:16 }}>Louvores do mês:</h3>
+      <h3 style={{ paddingLeft: 30, fontWeight: 400, fontSize: 18 }}>Louvores do mês:</h3>
       <div style={{ display: 'flex', overflow: 'auto', height: 80, gap: 6, padding: '0px 12px 12px 12px' }}>
         {
-          praises.sunday.map((pray) => {
+          uniqueItems.map((pray) => {
             return (
-              <div style={{ minWidth: 100, maxWidth:120, borderRadius: 12, backgroundColor: '#558B2F', display: 'grid', alignItems: 'center', justifyContent: 'center', padding: 6, }}>
-                <strong style={{ textAlign: 'center', fontWeight: 300, fontSize: 15, color: '#fff' }}>{pray}</strong>
-                <strong style={{ textAlign: 'center', fontWeight: 300, fontSize: 13, color: '#fff' }}>Tom: C#</strong>
+              <div style={{ position: 'relative', minWidth: 95, height: 40, borderRadius: 10, backgroundColor: '#00796B', display: 'grid', alignItems: 'center', justifyContent: 'center', padding: 8, }}>
+                <strong style={{ textAlign: 'center', fontWeight: 400, fontSize: 14, color: '#fff' }}>{pray}</strong>
+                <strong style={{ boxShadow: '1px 1px 5px #00000040', fontWeight: 500, position: 'absolute', bottom: -10, left: 6, backgroundColor: '#fff', fontSize: 14, paddingLeft: 8, paddingRight: 8, paddingTop: 1, paddingBottom: 1, borderRadius: 12 }}>{getTons(pray)}</strong>
               </div>
             )
           })
@@ -130,14 +142,12 @@ function ListPraises() {
   )
 }
 
+
+
 export default function App() {
   return (
     <div>
-
-      <div style={{ backgroundColor: '#fff', height: 60, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <span style={{ textAlign: 'center', fontSize: 28, fontWeight: 600 }}>Escala Louvor</span>
-
-      </div>
+      <h1 style={{ textAlign: 'center', fontSize: 28, fontWeight: 600, marginBottom: 70 }}>Escala Louvor</h1>
 
       <ListPraises />
       {sortedScales.map((scale, index) => {
@@ -145,6 +155,8 @@ export default function App() {
           <CalendarItem key={index} data={scale} />
         )
       })}
+
+      <p style={{ textAlign: 'center' }}>Ministério de Louvor Adonai</p>
     </div>
   );
 }
--- src/CalendarItem/index.js
+++ src/CalendarItem/index.js
@@ -25,24 +25,24 @@ export default function CalendarItem({ data }) {
 
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: -6, flexWrap: 'wrap' }}>
 
-                <h1 style={{ fontSize: 20 }}>{data.cult}</h1>
+                <h1 style={{ fontSize: 20, color:'#00796B' }}>{data.cult}</h1>
                 <span style={{ fontSize: 14 }}>{moment(data.date).format('DD/MM')}</span>
 
             </div>
             <div style={{ display: 'grid', marginBottom: 12, gap: 2 }}>
 
                 {data.praises?.map((pray, index) => {
-                    return <label key={index} style={{ fontSize: 15 }}>- {pray} {index === 0 && data.cult === 'Louvor e Pregação' ? ' [ Prelúdio ]' : ''}</label>
+                    return <label key={index} style={{ fontSize: 15, fontWeight: 300 }}>- {pray} {index === 0 && data.cult === 'Louvor e Pregação' ? ' [ Prelúdio ]' : ''}</label>
 
                 })}
                 {data.hcc?.map((hcc, index) => {
-                    return <label key={index} style={{ fontSize: 15 }}>CC {hcc} - {getName(hcc)} </label>
+                    return <label key={index} style={{ fontSize: 15, fontWeight: 300 }}>CC {hcc} - {getName(hcc)} </label>
 
                 })}
             </div>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', borderWidth:.5, borderTop:'1px solid #aaa', paddingTop:12 }}>
 
-                {data.vocals.map((vocal, index) => {
+                {data.vocals.concat(data.instrumentalists).map((vocal, index) => {
                     return <strong key={index} style={{ fontSize: 15, fontWeight: 300, paddingRight: 6 }}>{vocal}</strong>
 
                 })}
