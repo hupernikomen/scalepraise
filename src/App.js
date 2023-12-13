2d426bf852fac55d9115c080254c0dec39f1aa2f
Wilson Ramos <hupcontato@gmail.com>
2023-12-13 07:52:04 -0300

    Postagem

--- src/App.js
+++ src/App.js
@@ -1,225 +1,237 @@
 import moment from 'moment';
 import CalendarItem from './CalendarItem';
+import { useEffect, useState } from 'react';
 
 const listaLouvores = require('./utils/louvores.json');
 
-// Musicos
-const musicos = {
-  instrumentistas: {
-    sexta: ['Wesley', 'Warley'],
-    psh: ['Thiago', 'Andre', 'Warley', 'Rhuan', '[ Escolher ]'],
-    ebd: ['Annes']
-  },
+export default function App() {
+  const [louvoresTocador, setLouvoresTocador] = useState([]);
+  const [tipoLouvor, setTipoLouvor] = useState('');
+  const tocadores = ['Andre', 'Thiago', 'Warley', 'Rhuan'];
+  const [tocador, setTocador] = useState('');
 
-  vocalistas: {
-    sexta: ['Lidiane', 'Laís', 'Edvan'],
-    psh: [
-      ['Wilson', 'Paulinha'],
-      ['Kelviane', 'Fernanda'],
-      ['Edmilson', 'Fernanda'],
-      ['Lidiane', 'Duda', 'Laís'],
-      [('Fernanda', 'Paulinha')]
-    ],
-    ebd: ['Wilson', 'Paulinha'],
-    domingonoite: ['Laís', 'Kelviane', 'Lidiane', 'Duda', '[ Escolher ]']
-  }
-};
+  useEffect(() => {
+    buscaTocador(tocador, tipoLouvor);
+  }, [tocador, tipoLouvor]);
 
-// Revesamento do guitarrista com base no numero da semana
-// Revesamento de guitarrista para o domingo a noite
-const tocadorPSH = musicos.instrumentistas.psh[numeroDaSemana() - 1];
-const tocadorIbav = numeroDaSemana() % 2 === 0 ? 'André' : 'Thiago';
+  // Musicos
+  const musicos = {
+    instrumentistas: {
+      sexta: ['Wesley', 'Warley'],
+      psh: ['Thiago', 'Andre', 'Warley', 'Rhuan', '[ Escolher ]'],
+      ebd: ['Annes']
+    },
 
-const escalas = [
-  // Doutrina / Oração
-  {
-    data: proximaSexta(),
-    culto: 'Doutrina / Oração',
-    louvores: [
-      buscaLouvores('firstmoment', 'Warley', listaLouvores)[numeroDaSemana() - 1]?.louvor ||
-        buscaLouvores('firstmoment', 'Warley', listaLouvores)[0]?.louvor,
-      buscaLouvores('firstmoment', 'Warley', listaLouvores)[numeroDaSemana()]?.louvor ||
-        buscaLouvores('firstmoment', 'Warley', listaLouvores)[1]?.louvor
-    ],
-    vocalistas: musicos.vocalistas.sexta,
-    instrumentistas: musicos.instrumentistas.sexta
-  },
-
-  // EBD
-  {
-    data: proximoDomingo(),
-    culto: numeroDeDomingosMes() === numeroDaSemana() ? 'EBD com Ceia' : 'EBD',
-    louvores: [],
-    vocalistas: musicos.vocalistas.ebd,
-    instrumentistas: musicos.instrumentistas.ebd
-  },
-
-  // Culto de Louvor e Pregação
-  {
-    data: proximoDomingo(),
-    culto: 'Louvor e Pregação',
-    louvores: [
-      buscaLouvores('prelude', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor,
-      buscaLouvores('firstmoment', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor,
-      buscaLouvores('communion', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor
-    ],
-    hcc: [],
-    vocalistas: [
-      'Edmilson',
-      'Edvan',
-      numeroDaSemana() % 2 === 0 ? 'Fernanda' : 'Paulinha',
-      musicos.vocalistas.domingonoite[numeroDaSemana() - 1]
-    ],
-    instrumentistas: ['Wesley', 'Annes', tocadorIbav]
-  },
-
-  // Culto PSH
-  {
-    data: proximoSabado(),
-    culto: 'PSH',
-    louvores: [
-      buscaLouvores('prelude', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor,
-      buscaLouvores('firstmoment', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor,
-      buscaLouvores('communion', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor
-    ],
-    vocalistas: musicos.vocalistas.psh[numeroDaSemana() - 1],
-    instrumentistas: musicos.instrumentistas.psh[numeroDaSemana() - 1]
-  },
-  // ________________________________________
-
-  {
-    data: converteData('29/12/2023'),
-    culto: 'Culto Oração Especial',
-    louvores: [''],
-    vocalistas: ['Edvan', 'Laís', 'Lidiane'],
-    instrumentistas: ['Wesley', 'Warley']
-  },
-  {
-    data: converteData('24/12/2023'),
-    culto: 'Culto Especial Natal',
-    louvores: [
-      'Bom estarmos aqui',
-      'Isaias 9',
-      'Nada além do sangue',
-      'Agnus Dei',
-      'CC 30 - Noite de paz'
-    ],
-    vocalistas: ['Edmilson', 'Edvan', 'Fernanda', 'Laís', 'Paulinha'],
-    instrumentistas: ['Wesley', 'Warley', 'André', 'Annes']
-  },
-  {
-    data: converteData('31/12/2023'),
-    culto: 'Culto Especial Ano Novo',
-    louvores: [
-      'Renova-me',
-      'Nada além do sangue',
-      'Agnus Dei',
-      'CC 560 - Ano Novo',
-      'CC 456 - O Estandarte'
-    ],
-    vocalistas: ['Wilson', 'Kelviane', 'Duda', 'Lidiane', 'Paulinha'],
-    instrumentistas: ['Wesley', 'André', 'Thiago', 'Annes']
-  }
-];
-
-function buscaLouvores(tipo, tocador, lista) {
-  let resultado = [];
-
-  lista.forEach((item) => {
-    if (item?.tocadores?.indexOf(tocador) && item?.tipo === tipo) {
-      resultado.push(item);
+    vocalistas: {
+      sexta: ['Lidiane', 'Laís', 'Edvan'],
+      psh: [['Wilson', 'Paulinha'], ['Kelviane', 'Fernanda'], ['Edmilson', 'Fernanda'], ['Lidiane', 'Duda', 'Laís'], [('Fernanda', 'Paulinha')]],
+      ebd: ['Wilson', 'Paulinha'],
+      domingonoite: ['Laís', 'Kelviane', 'Lidiane', 'Duda', '[ Escolher ]']
     }
+  };
+
+  // Revesamento do guitarrista com base no numero da semana
+  // Revesamento de guitarrista para o domingo a noite
+  const tocadorPSH = musicos.instrumentistas.psh[numeroDaSemana() - 1];
+  const tocadorIbav = numeroDaSemana() % 2 === 0 ? 'André' : 'Thiago';
+
+  const escalas = [
+    // Doutrina / Oração
+    {
+      data: proximaSexta(),
+      culto: 'Doutrina / Oração',
+      louvores: [
+        buscaLouvores('firstmoment', 'Warley', listaLouvores)[numeroDaSemana() - 1]?.louvor || buscaLouvores('firstmoment', 'Warley', listaLouvores)[0]?.louvor,
+        buscaLouvores('firstmoment', 'Warley', listaLouvores)[numeroDaSemana()]?.louvor || buscaLouvores('firstmoment', 'Warley', listaLouvores)[1]?.louvor
+      ],
+      vocalistas: musicos.vocalistas.sexta,
+      instrumentistas: musicos.instrumentistas.sexta
+    },
+
+    // EBD
+    {
+      data: proximoDomingo(),
+      culto: numeroDeDomingosMes() === numeroDaSemana() ? 'EBD com Ceia' : 'EBD',
+      louvores: [],
+      vocalistas: musicos.vocalistas.ebd,
+      instrumentistas: musicos.instrumentistas.ebd
+    },
+
+    // Culto de Louvor e Pregação
+    {
+      data: proximoDomingo(),
+      culto: 'Louvor e Pregação',
+      louvores: [
+        buscaLouvores('prelude', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor,
+        buscaLouvores('firstmoment', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor,
+        buscaLouvores('communion', tocadorIbav, listaLouvores)[numeroDaSemana()]?.louvor
+      ],
+      hcc: [],
+      vocalistas: ['Edmilson', 'Edvan', numeroDaSemana() % 2 === 0 ? 'Fernanda' : 'Paulinha', musicos.vocalistas.domingonoite[numeroDaSemana() - 1]],
+      instrumentistas: ['Wesley', 'Annes', tocadorIbav]
+    },
+
+    // Culto PSH
+    {
+      data: proximoSabado(),
+      culto: 'PSH',
+      louvores: [
+        buscaLouvores('prelude', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor,
+        buscaLouvores('firstmoment', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor,
+        buscaLouvores('communion', tocadorPSH, listaLouvores)[numeroDaSemana() - 1]?.louvor
+      ],
+      vocalistas: musicos.vocalistas.psh[numeroDaSemana() - 1],
+      instrumentistas: musicos.instrumentistas.psh[numeroDaSemana() - 1]
+    },
+    // ________________________________________
+
+    {
+      data: converteData('29/12/2023'),
+      culto: 'Culto Oração Especial',
+      louvores: [''],
+      vocalistas: ['Edvan', 'Laís', 'Lidiane'],
+      instrumentistas: ['Wesley', 'Warley']
+    },
+    {
+      data: converteData('24/12/2023'),
+      culto: 'Culto Especial Natal',
+      louvores: ['Bom estarmos aqui', 'Isaias 9', 'Nada além do sangue', 'Agnus Dei', 'CC 30 - Noite de paz'],
+      vocalistas: ['Edmilson', 'Edvan', 'Fernanda', 'Laís', 'Paulinha'],
+      instrumentistas: ['Wesley', 'Warley', 'André', 'Annes']
+    },
+    {
+      data: converteData('31/12/2023'),
+      culto: 'Culto Especial Ano Novo',
+      louvores: ['Renova-me', 'Nada além do sangue', 'Agnus Dei', 'CC 560 - Ano Novo', 'CC 456 - O Estandarte'],
+      vocalistas: ['Wilson', 'Kelviane', 'Duda', 'Lidiane', 'Paulinha'],
+      instrumentistas: ['Wesley', 'André', 'Thiago', 'Annes']
+    }
+  ];
+
+  function buscaLouvores(tipo, tocador, lista) {
+    let resultado = [];
+
+    lista.forEach((item) => {
+      if (item?.tocadores?.indexOf(tocador) && item?.tipo === tipo) {
+        resultado.push(item);
+      }
+    });
+
+    return resultado;
+  }
+
+  //Converte data String em data Date
+  function converteData(inData) {
+    // Divide a string da data em dia, mês e ano
+    const [day, month, year] = inData.split('/');
+
+    // Criar um objeto de data JavaScript com o dia, mês e ano
+    const data = new Date(year, month - 1, day);
+    return data;
+  }
+
+  // Encontre o próximo dia que seja sexta-feira
+  function proximaSexta() {
+    let now = new Date();
+    let nextFriday = new Date(now.getTime());
+    while (nextFriday.getDay() !== 5) {
+      nextFriday.setDate(nextFriday.getDate() + 1);
+    }
+    return nextFriday;
+  }
+
+  // Encontre o próximo dia que seja sexta-feira
+  function proximoSabado() {
+    let now = new Date();
+    let nextFriday = new Date(now.getTime());
+    while (nextFriday.getDay() !== 6) {
+      nextFriday.setDate(nextFriday.getDate() + 1);
+    }
+
+    return nextFriday;
+  }
+
+  // Encontre o próximo dia que seja domingo
+  function proximoDomingo() {
+    let now = new Date();
+    let nextSunday = new Date(now.getTime());
+    while (nextSunday.getDay() !== 0) {
+      nextSunday.setDate(nextSunday.getDate() + 1);
+    }
+    return nextSunday;
+  }
+
+  function numeroDaSemana() {
+    // Retorna o número da semana com base na data atual
+    let today = new Date();
+    let primeiroDiaDoMes = new Date(today.getFullYear(), today.getMonth(), 1);
+    let diasPassadosDoMes = today.getDate() - 1;
+    let numeroDaSemana = Math.ceil((diasPassadosDoMes + primeiroDiaDoMes.getDay() + 1) / 7);
+    return numeroDaSemana;
+  }
+
+  function numeroDeDomingosMes() {
+    // Retorna o número de domingos em um mês específico
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let domingos = 0;
+
+    for (let day = 1; day <= 31; day++) {
+      let data = new Date(year, month, day);
+      if (data.getMonth() === month && data.getDay() === 0) {
+        domingos++;
+      }
+    }
+
+    return domingos;
+  }
+
+  // Coloca as datas em ordem cronologica
+  const ordenaEscala = escalas.sort((a, b) => {
+    return moment(a.data) - moment(b.data);
   });
 
-  return resultado;
-}
-
-//Converte data String em data Date
-function converteData(inData) {
-  // Divide a string da data em dia, mês e ano
-  const [day, month, year] = inData.split('/');
-
-  // Criar um objeto de data JavaScript com o dia, mês e ano
-  const data = new Date(year, month - 1, day);
-  return data;
-}
-
-// Encontre o próximo dia que seja sexta-feira
-function proximaSexta() {
-  let now = new Date();
-  let nextFriday = new Date(now.getTime());
-  while (nextFriday.getDay() !== 5) {
-    nextFriday.setDate(nextFriday.getDate() + 1);
-  }
-  return nextFriday;
-}
-
-// Encontre o próximo dia que seja sexta-feira
-function proximoSabado() {
-  let now = new Date();
-  let nextFriday = new Date(now.getTime());
-  while (nextFriday.getDay() !== 6) {
-    nextFriday.setDate(nextFriday.getDate() + 1);
+  // Busca o Tocador com base no tipo de louvor
+  function buscaTocador(tocador, tipo) {
+    const louvorEncontrado = listaLouvores.filter((louvor) => louvor.tocadores?.includes(tocador) && louvor.tipo === tipo);
+    setLouvoresTocador(louvorEncontrado.map((louvor) => louvor.louvor));
   }
 
-  return nextFriday;
-}
-
-// Encontre o próximo dia que seja domingo
-function proximoDomingo() {
-  let now = new Date();
-  let nextSunday = new Date(now.getTime());
-  while (nextSunday.getDay() !== 0) {
-    nextSunday.setDate(nextSunday.getDate() + 1);
-  }
-  return nextSunday;
-}
-
-function numeroDaSemana() {
-  // Retorna o número da semana com base na data atual
-  let today = new Date();
-  let primeiroDiaDoMes = new Date(today.getFullYear(), today.getMonth(), 1);
-  let diasPassadosDoMes = today.getDate() - 1;
-  let numeroDaSemana = Math.ceil((diasPassadosDoMes + primeiroDiaDoMes.getDay() + 1) / 7);
-  return numeroDaSemana;
-}
-
-function numeroDeDomingosMes() {
-  // Retorna o número de domingos em um mês específico
-  let today = new Date();
-  let month = today.getMonth();
-  let year = today.getFullYear();
-  let domingos = 0;
-
-  for (let day = 1; day <= 31; day++) {
-    let data = new Date(year, month, day);
-    if (data.getMonth() === month && data.getDay() === 0) {
-      domingos++;
-    }
-  }
-
-  return domingos;
-}
-
-// Coloca as datas em ordem cronologica
-const ordenaEscala = escalas.sort((a, b) => {
-  return moment(a.data) - moment(b.data);
-});
-
-export default function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center', fontSize: 24, fontWeight: 800 }}>
-        ESCALA DE LOUVOR ADONAI
-      </h1>
+    <div style={{ padding: 12 }}>
+      <h1 style={{ marginLeft: 18, fontSize: 24, fontWeight: 800 }}>ESCALA DE LOUVOR ADONAI</h1>
 
-      {ordenaEscala.map((scale, index) => {
-        return <CalendarItem key={index} data={scale} />;
-      })}
+      <div>
+        <label style={{ margin: '18px' }}>Encontre Louvores</label>
+        <div style={{ display: 'flex', margin: '6px' }}>
+          <select style={{ height: 28, padding: '6px', borderRadius: 6, border: 0, outline: 'none' }} onChange={(e) => setTocador(e.target.value)}>
+            <option>Tocador</option>
+            {tocadores.map((tocador) => (
+              <option>{tocador}</option>
+            ))}
+          </select>
 
-      <p style={{ textAlign: 'center', marginTop: 50, fontWeight: 300 }}>
-        Ministério de Louvor Adonai
-      </p>
+          <select style={{ height: 28, padding: '6px', borderRadius: 6, border: 0, outline: 'none' }} onChange={(e) => setTipoLouvor(e.target.value)}>
+            <option>Tipo</option>
+            <option value={'prelude'}>Prelúdio</option>
+            <option value={'firstmoment'}>Primeiro Momento</option>
+            <option value={'communion'}>Comunhão</option>
+          </select>
+        </div>
+
+        <div style={{ margin: '18px 0' }}>
+          {louvoresTocador.map((louvor) => (
+            <div style={{ borderLeft: '4px solid #795548', fontSize: 15, fontWeight: 300, paddingLeft: '12px', margin: '2px 12px', backgroundColor: '#fff' }}>{louvor}</div>
+          ))}
+        </div>
+      </div>
+
+      {ordenaEscala.map((scale, index) => (
+        <CalendarItem key={index} data={scale} />
+      ))}
+
+      <p style={{ textAlign: 'center', marginTop: 50, fontWeight: 300 }}>Ministério de Louvor Adonai</p>
     </div>
   );
 }
--- src/CalendarItem/index.js
+++ src/CalendarItem/index.js
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
 import './estilo.css';
 import moment from 'moment';
-const escala = require('../utils/louvores.json');
 
 export default function CalendarItem({ data }) {
-  const tocadores = ['André', 'Thiago', 'Warley', 'Rhuan'];
-  const [clicado, setClicado] = useState(false);
-  const [tocador, setTocador] = useState('');
-  const [louvoresTocador, setLouvoresTocador] = useState([]);
-  const [tipoLouvor, setTipoLouvor] = useState('');
-
-  useEffect(() => {
-    buscaTocador(tocador, tipoLouvor);
-  }, [tocador, tipoLouvor]);
-
+  const escala = require('../utils/louvores.json');
   const semana = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'];
-  if (moment(data.date).isBefore(moment(), 'day')) {
+
+  if (moment(data.data).isBefore(moment(), 'day')) {
     return null;
   }
 
@@ -25,14 +15,6 @@ export default function CalendarItem({ data }) {
       if (escala[i]?.num === num) return escala[i].louvor;
     }
     return null; // caso não encontre, retorna null
-  }
-
-  // Busca o Tocador com base no tipo de louvor
-  function buscaTocador(tocador, tipo) {
-    const louvorEncontrado = escala.filter(
-      (louvor) => louvor.tocadores?.includes(tocador) && louvor.tipo === tipo
-    );
-    setLouvoresTocador(louvorEncontrado.map((louvor) => louvor.louvor));
   }
 
   return (
@@ -45,7 +27,6 @@ export default function CalendarItem({ data }) {
         <div className="date">
           <span
             style={{
-              fontSize: 16,
               fontWeight: 600,
               color: data.culto === 'PSH' ? '#795548' : '#00796B'
             }}
@@ -65,10 +46,7 @@ export default function CalendarItem({ data }) {
           </span>
         </div>
       </div>
-      <div
-        onClick={() => setClicado(!clicado)}
-        style={{ display: 'grid', marginBottom: 18, gap: 2 }}
-      >
+      <div style={{ display: 'grid', marginBottom: 18, gap: 2 }}>
         {data?.louvores?.map((louvor, index) => {
           const tom = escala.find((item) => item.louvor === louvor);
 
@@ -94,7 +72,7 @@ export default function CalendarItem({ data }) {
                   }}
                 >
                   <label style={{ color: '#aaa', marginRight: 4 }}>♪</label>
-                  <strong>{tom?.tom}</strong>
+                  <div style={{ fontWeight: 500, color: '#795548' }}>{tom?.tom}</div>
                 </div>
               ) : null}
             </label>
@@ -126,36 +104,6 @@ export default function CalendarItem({ data }) {
           );
         })}
       </div>
-
-      {clicado ? (
-        <div style={{ marginTop: 12 }}>
-          <div style={{ display: 'flex', gap: 12 }}>
-            <select
-              style={{ height: 28, padding: '0px 6px', borderRadius: 6, backgroundColor: '#fff' }}
-              onChange={(e) => setTocador(e.target.value)}
-            >
-              {tocadores.map((guitarist) => (
-                <option>{guitarist}</option>
-              ))}
-            </select>
-
-            <select
-              style={{ height: 28, padding: '0px 6px', borderRadius: 6, backgroundColor: '#fff' }}
-              onChange={(e) => setTipoLouvor(e.target.value)}
-            >
-              <option value={'prelude'}>Prelúdio</option>
-              <option value={'firstmoment'}>Primeiro Momento</option>
-              <option value={'communion'}>Comunhão</option>
-            </select>
-          </div>
-
-          <div style={{ marginTop: 12 }}>
-            {louvoresTocador.map((praise) => {
-              return <div style={{ fontSize: 15, fontWeight: 300 }}>- {praise}</div>;
-            })}
-          </div>
-        </div>
-      ) : null}
     </div>
   );
 }
